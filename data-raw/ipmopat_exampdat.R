c41e61cc87e862f60718c4f4e0ffd57bbd9c11a9
vanichols <virginia.nichols@gmail.com>
2025-02-10 12:49:05 +0100

    fix exampl data

--- data-raw/ipmopat_exampdat.R
+++ data-raw/ipmopat_exampdat.R
@@ -7,12 +7,30 @@ library(tidyr)
 
 # 1. read in data ---------------------------------------------------------
 
-impopat_exampdat <-
+d1 <-
   read_delim("data-raw/ABC-fairy dust lettuce.csv", delim = ";") %>%
   janitor::remove_empty() %>%
   fill(title)
 
+
+# 2. make it simpler ------------------------------------------------------
+
+d2 <-
+  d1 %>%
+  select(-question)
+
+# 3. make a separate example with quetsions ------------------------------------------------------
+
+d3 <-
+  d1 %>%
+  select(short, question)
+
+
 # internal and external data ----------------------------------------------
 
+impopat_exampdat <- d2
 usethis::use_data(impopat_exampdat, overwrite = TRUE)
-usethis::use_data(impopat_exampdat, internal = TRUE)
+usethis::use_data(impopat_exampdat, internal = TRUE, overwrite = TRUE)
+
+impopat_shortinfo <- d3
+usethis::use_data(impopat_shortinfo, overwrite = TRUE)
